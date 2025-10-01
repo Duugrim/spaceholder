938a64e92c3dc512b3b6e56a59b39dd7c552b9d7
Duugrim <t.g.grovin@gmail.com>
2025-10-02 00:23:56 +0300

    Initial commit: import existing boilerplate system

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const SPACEHOLDER = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+SPACEHOLDER.abilities = {
+  str: 'SPACEHOLDER.Ability.Str.long',
+  dex: 'SPACEHOLDER.Ability.Dex.long',
+  con: 'SPACEHOLDER.Ability.Con.long',
+  int: 'SPACEHOLDER.Ability.Int.long',
+  wis: 'SPACEHOLDER.Ability.Wis.long',
+  cha: 'SPACEHOLDER.Ability.Cha.long',
+};
+
+SPACEHOLDER.abilityAbbreviations = {
+  str: 'SPACEHOLDER.Ability.Str.abbr',
+  dex: 'SPACEHOLDER.Ability.Dex.abbr',
+  con: 'SPACEHOLDER.Ability.Con.abbr',
+  int: 'SPACEHOLDER.Ability.Int.abbr',
+  wis: 'SPACEHOLDER.Ability.Wis.abbr',
+  cha: 'SPACEHOLDER.Ability.Cha.abbr',
+};
