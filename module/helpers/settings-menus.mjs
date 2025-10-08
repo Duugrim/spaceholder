c903a5cbb13b7203422802ff828080b0e32efe42
Duugrim <t.g.grovin@gmail.com>
2025-10-08 22:28:45 +0300

    Migrate settings submenus and token pointer-rotator to Application V2

--- module/helpers/settings-menus.mjs
+++ module/helpers/settings-menus.mjs
@@ -1,34 +1,62 @@
-// Settings menus (submenus) for SpaceHolder
+// Settings menus (submenus) for SpaceHolder (Application V2)
 
 const MODULE_NS = 'spaceholder';
 
-class BaseSettingsForm extends FormApplication {
-  static get defaultOptions() {
-    return foundry.utils.mergeObject(super.defaultOptions, {
-      width: 480,
-      height: 'auto',
-      closeOnSubmit: true,
+class V2Base extends foundry.applications.api.HandlebarsApplicationMixin(foundry.applications.api.ApplicationV2) {
+  static DEFAULT_OPTIONS = {
+    ...super.DEFAULT_OPTIONS,
+    tag: 'form',
+    classes: ['spaceholder', 'settings-form'],
+    position: { width: 480, height: 'auto' },
+    form: {
+      handler: null, // to be set by subclasses
       submitOnChange: false,
-      submitOnClose: false,
-      resizable: false,
-      classes: ['spaceholder', 'settings-form'],
-    });
-  }
+      closeOnSubmit: true,
+    },
+  };
 
-  async _updateSettings(map) {
+  static async _applySettings(map) {
     for (const [key, value] of Object.entries(map)) {
       await game.settings.set(MODULE_NS, key, value);
     }
   }
 }
 
-export class TokenPointerSettingsApp extends BaseSettingsForm {
-  static get defaultOptions() {
-    const opts = super.defaultOptions;
-    return { ...opts, id: 'spaceholder-tokenpointer-settings', title: 'Token Pointer Settings', template: 'systems/spaceholder/templates/settings/tokenpointer.hbs' };
+export class TokenPointerSettingsApp extends V2Base {
+  static DEFAULT_OPTIONS = {
+    ...super.DEFAULT_OPTIONS,
+    id: 'spaceholder-tokenpointer-settings',
+    window: { title: 'Token Pointer Settings' },
+  };
+
+  static PARTS = {
+    form: { template: 'systems/spaceholder/templates/settings/tokenpointer.hbs' },
+  };
+
+  static async onSubmit(event, form, formData) {
+    const data = formData.object; // flattened object
+    const map = {};
+    map['tokenpointer.color'] = String(data['tokenpointer.color']);
+    map['tokenpointer.distance'] = Number(data['tokenpointer.distance']);
+    map['tokenpointer.scale'] = Number(data['tokenpointer.scale']);
+    map['tokenpointer.mode'] = Number(data['tokenpointer.mode']);
+    map['tokenpointer.combatOnly'] = !!data['tokenpointer.combatOnly'];
+    map['tokenpointer.hideOnDead'] = !!data['tokenpointer.hideOnDead'];
+    map['tokenpointer.lockToGrid'] = !!data['tokenpointer.lockToGrid'];
+    map['tokenpointer.flipHorizontal'] = !!data['tokenpointer.flipHorizontal'];
+    map['tokenpointer.pointerType'] = String(data['tokenpointer.pointerType']);
+    await V2Base._applySettings(map);
   }
 
-  async getData() {
+  static DEFAULT_OPTIONS_FORM = (() => {
+    const opts = foundry.utils.deepClone(this.DEFAULT_OPTIONS);
+    opts.form.handler = this.onSubmit;
+    return opts;
+  })();
+
+  static get defaultOptions() { return this.DEFAULT_OPTIONS_FORM; }
+
+  async _prepareContext() {
     return {
       color: (game.settings.get(MODULE_NS, 'tokenpointer.color')?.css) ?? '#000000',
       distance: game.settings.get(MODULE_NS, 'tokenpointer.distance'),
@@ -41,45 +69,44 @@ export class TokenPointerSettingsApp extends BaseSettingsForm {
       pointerType: game.settings.get(MODULE_NS, 'tokenpointer.pointerType'),
     };
   }
-
-  async _updateObject(_event, formData) {
-    const map = {};
-    // normalize data
-    map['tokenpointer.color'] = String(formData['tokenpointer.color']);
-    map['tokenpointer.distance'] = Number(formData['tokenpointer.distance']);
-    map['tokenpointer.scale'] = Number(formData['tokenpointer.scale']);
-    map['tokenpointer.mode'] = Number(formData['tokenpointer.mode']);
-    map['tokenpointer.combatOnly'] = !!formData['tokenpointer.combatOnly'];
-    map['tokenpointer.hideOnDead'] = !!formData['tokenpointer.hideOnDead'];
-    map['tokenpointer.lockToGrid'] = !!formData['tokenpointer.lockToGrid'];
-    map['tokenpointer.flipHorizontal'] = !!formData['tokenpointer.flipHorizontal'];
-    map['tokenpointer.pointerType'] = String(formData['tokenpointer.pointerType']);
-    await this._updateSettings(map);
-  }
 }
 
-export class TokenRotatorSettingsApp extends BaseSettingsForm {
-  static get defaultOptions() {
-    const opts = super.defaultOptions;
-    return { ...opts, id: 'spaceholder-tokenrotator-settings', title: 'Token Rotator Settings', template: 'systems/spaceholder/templates/settings/tokenrotator.hbs' };
+export class TokenRotatorSettingsApp extends V2Base {
+  static DEFAULT_OPTIONS = {
+    ...super.DEFAULT_OPTIONS,
+    id: 'spaceholder-tokenrotator-settings',
+    window: { title: 'Token Rotator Settings' },
+  };
+
+  static PARTS = {
+    form: { template: 'systems/spaceholder/templates/settings/tokenrotator.hbs' },
+  };
+
+  static async onSubmit(event, form, formData) {
+    const data = formData.object;
+    const map = {};
+    map['tokenrotator.altSnapByDefault'] = !!data['tokenrotator.altSnapByDefault'];
+    map['tokenrotator.smoothRotation'] = !!data['tokenrotator.smoothRotation'];
+    map['tokenrotator.fastPreview'] = !!data['tokenrotator.fastPreview'];
+    map['tokenrotator.rotationUpdateFrequency'] = Number(data['tokenrotator.rotationUpdateFrequency']);
+    await V2Base._applySettings(map);
   }
 
-  async getData() {
+  static DEFAULT_OPTIONS_FORM = (() => {
+    const opts = foundry.utils.deepClone(this.DEFAULT_OPTIONS);
+    opts.form.handler = this.onSubmit;
+    return opts;
+  })();
+
+  static get defaultOptions() { return this.DEFAULT_OPTIONS_FORM; }
+
+  async _prepareContext() {
     return {
       altSnapByDefault: game.settings.get(MODULE_NS, 'tokenrotator.altSnapByDefault'),
       smoothRotation: game.settings.get(MODULE_NS, 'tokenrotator.smoothRotation'),
       fastPreview: game.settings.get(MODULE_NS, 'tokenrotator.fastPreview'),
       rotationUpdateFrequency: game.settings.get(MODULE_NS, 'tokenrotator.rotationUpdateFrequency'),
     };
-  }
-
-  async _updateObject(_event, formData) {
-    const map = {};
-    map['tokenrotator.altSnapByDefault'] = !!formData['tokenrotator.altSnapByDefault'];
-    map['tokenrotator.smoothRotation'] = !!formData['tokenrotator.smoothRotation'];
-    map['tokenrotator.fastPreview'] = !!formData['tokenrotator.fastPreview'];
-    map['tokenrotator.rotationUpdateFrequency'] = Number(formData['tokenrotator.rotationUpdateFrequency']);
-    await this._updateSettings(map);
   }
 }
 
